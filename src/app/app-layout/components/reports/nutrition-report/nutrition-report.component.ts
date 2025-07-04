13a510749e4d243a449eee699539f408faa0da30
Gareth Deeney <gareth.deene@googlemail.com>
2025-07-04 15:18:27 +0100

    fix: tidy up report styling

--- src/app/app-layout/components/reports/nutrition-report/nutrition-report.component.ts
+++ src/app/app-layout/components/reports/nutrition-report/nutrition-report.component.ts
@@ -1,53 +1,122 @@
 import { Component, Input, OnInit } from '@angular/core';
 import Chart from 'chart.js/auto';
 import { ReportService } from '../reports.service';
+import { NutritionGroupComponent } from '../../child/child-details/nutrition-group/nutrition-group.component';
 
 @Component({
   selector: 'nutrition-report',
-  template: ` <div style="width: 80%;">
-    <canvas id="{{ chartId }}"> </canvas>
-  </div>`,
-  styleUrls: [],
+  template: `
+    <div class="nutrition-reports">
+      <h2>Nutrition Reports</h2>
+
+      <div class="chart-row">
+        <div class="chart-box">
+          <canvas id="caloriesChart"> </canvas>
+        </div>
+
+        <div class="chart-box">
+          <canvas id="energyChart"> </canvas>
+        </div>
+
+        <div class="chart-box">
+          <canvas id="fatChart"> </canvas>
+        </div>
+      </div>
+
+      <div class="chart-row">
+        <div class="chart-box">
+          <canvas id="saturatesChart"> </canvas>
+        </div>
+
+        <div class="chart-box">
+          <canvas id="saltChart"> </canvas>
+        </div>
+
+        <div class="chart-box">
+          <canvas id="sugarChart"> </canvas>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: `
+
+ .nutrition-reports {
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   margin-top: 1rem;
+   width: 98%;
+   gap: 0.25rem;
+ }
+
+ .chart-row {
+   display: flex;
+   gap: 1rem;
+   width: 100%;
+   justify-content: center;
+ }
+
+ .chart-box {
+   width: 30%;
+   border: 1px solid lightgray;
+   border-radius: 15px;
+   padding: 0.5rem;
+ }
+  `,
   standalone: false,
 })
 export class NutritionReportComponent implements OnInit {
   constructor(protected reportService: ReportService) {}
-  @Input() nutritionType: string = '';
-  @Input() chartId: string = 'chartId';
+
+  setDataAndChart(obj: any, nutritionType: string, chartId: string) {
+    const data = [
+      { month: 'Jan', count: obj.january[nutritionType.toLowerCase()] },
+      { month: 'Feb', count: obj.february[nutritionType.toLowerCase()] },
+      { month: 'Mar', count: obj.march[nutritionType.toLowerCase()] },
+      { month: 'Apr', count: obj.april[nutritionType.toLowerCase()] },
+      { month: 'May', count: obj.may[nutritionType.toLowerCase()] },
+      { month: 'June', count: obj.june[nutritionType.toLowerCase()] },
+      { month: 'July', count: obj.july[nutritionType.toLowerCase()] },
+      { month: 'Aug', count: obj.august[nutritionType.toLowerCase()] },
+      { month: 'Sept', count: obj.september[nutritionType.toLowerCase()] },
+      { month: 'Oct', count: obj.october[nutritionType.toLowerCase()] },
+      { month: 'Nov', count: obj.november[nutritionType.toLowerCase()] },
+      { month: 'Dec', count: obj.december[nutritionType.toLowerCase()] },
+    ];
+
+    let labelType;
+
+    if (nutritionType == 'Calories') {
+      labelType = 'kcal';
+    } else if (nutritionType == 'Energy') {
+      labelType = 'kj';
+    } else {
+      labelType = 'g';
+    }
+
+    return new Chart(chartId, {
+      type: 'bar',
+      data: {
+        labels: data.map((row) => row.month),
+        datasets: [
+          {
+            label: `Average intake of ${nutritionType} (${labelType})`,
+            data: data.map((row) => row.count),
+          },
+        ],
+      },
+    });
+  }
 
   ngOnInit(): void {
-    this.reportService.getCostReport$().subscribe(console.log);
     this.reportService.getNutritionReport$().subscribe((val) => {
-      const data = [
-        { month: 'Jan', count: val.january[this.nutritionType.toLowerCase()] },
-        { month: 'Feb', count: val.february[this.nutritionType.toLowerCase()] },
-        { month: 'Mar', count: val.march[this.nutritionType.toLowerCase()] },
-        { month: 'Apr', count: val.april[this.nutritionType.toLowerCase()] },
-        { month: 'May', count: val.may[this.nutritionType.toLowerCase()] },
-        { month: 'June', count: val.june[this.nutritionType.toLowerCase()] },
-        { month: 'July', count: val.july[this.nutritionType.toLowerCase()] },
-        { month: 'Aug', count: val.august[this.nutritionType.toLowerCase()] },
-        {
-          month: 'Sept',
-          count: val.september[this.nutritionType.toLowerCase()],
-        },
-        { month: 'Oct', count: val.october[this.nutritionType.toLowerCase()] },
-        { month: 'Nov', count: val.november[this.nutritionType.toLowerCase()] },
-        { month: 'Dec', count: val.december[this.nutritionType.toLowerCase()] },
-      ];
-
-      new Chart(this.chartId, {
-        type: 'bar',
-        data: {
-          labels: data.map((row) => row.month),
-          datasets: [
-            {
-              label: `Average intake of ${this.nutritionType}`,
-              data: data.map((row) => row.count),
-            },
-          ],
-        },
-      });
+      this.setDataAndChart(val, 'Calories', 'caloriesChart');
+      this.setDataAndChart(val, 'Energy', 'energyChart');
+      this.setDataAndChart(val, 'Fat', 'fatChart');
+      this.setDataAndChart(val, 'Saturates', 'saturatesChart');
+      this.setDataAndChart(val, 'Salt', 'saltChart');
+      this.setDataAndChart(val, 'Sugars', 'sugarChart');
     });
   }
 }
--- src/app/app-layout/components/reports/reports-hub/reports.component.ts
+++ src/app/app-layout/components/reports/reports-hub/reports.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./reports.component.scss'],
   standalone: false,
 })
-export class ReportsComponent {}
+export class ReportsComponent {
+  nutritionSlides = [
+    { chartId: '1', nutritionType: 'Calories' },
+    { chartId: '2', nutritionType: 'Energy' },
+    { chartId: '3', nutritionType: 'Fat' },
+    { chartId: '4', nutritionType: 'Saturates' },
+    { chartId: '5', nutritionType: 'Salt' },
+    { chartId: '6', nutritionType: 'Sugars' },
+  ];
+
+}
