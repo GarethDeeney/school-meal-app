40b3f50be1111c72d3a2209f40b0c9d4537fb9df
Gareth Deeney <gareth.deene@googlemail.com>
2025-07-04 10:35:38 +0100

    fix: tidy up

--- src/app/app-layout/components/reports/reports-hub/reports.component.ts
+++ src/app/app-layout/components/reports/reports-hub/reports.component.ts
@@ -8,8 +8,4 @@ import Chart from 'chart.js/auto';
   styleUrls: ['./reports.component.scss'],
   standalone: false,
 })
-export class ReportsComponent {
-  constructor(protected reportService: ReportService) {
-    this.reportService.getNutritionReport$().subscribe(console.log);
-  }
-}
+export class ReportsComponent {}
--- src/app/app-layout/components/reports/reports.service.ts
+++ src/app/app-layout/components/reports/reports.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Menu } from 'src/app/models/menu';
-import { Nutrition } from 'src/app/models/Nutrition';
 
 @Injectable({ providedIn: 'root' })
 export class ReportService {
