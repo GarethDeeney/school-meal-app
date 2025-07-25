81ad381021a3a331800f49d044ad78ac1b49e2c6
Gareth Deeney <gareth.deene@googlemail.com>
2025-07-25 15:26:35 +0100

    fix: remove unused code and tidy up

--- src/app/app-layout/log-in/log-in.component.ts
+++ src/app/app-layout/log-in/log-in.component.ts
@@ -4,10 +4,10 @@ import { Router } from '@angular/router';
 import { HttpService } from 'src/app/services/http.service';
 
 @Component({
-    selector: 'app-log-in',
-    templateUrl: './log-in.component.html',
-    styleUrls: ['./log-in.component.scss'],
-    standalone: false
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.scss'],
+  standalone: false,
 })
 export class LogInComponent {
   constructor(private http: HttpService, private router: Router) {}
@@ -16,41 +16,7 @@ export class LogInComponent {
     password: new FormControl(),
   });
 
-  // checkLoginCredentials = (
-  //   username: FormControl,
-  //   enteredPassword: FormControl
-  // ): boolean => {
-  //   this.getStaffDetailsbyName(username.value)?.subscribe((res) => {
-  //     this.user = res;
-  //     if (this.user[0]._id !== undefined) {
-  //       localStorage.setItem('id', this.user[0]._id);
-  //     }
-  //   });
-
-  //   if (username.value == null || enteredPassword.value == null) {
-  //     enteredPassword.setErrors({
-  //       notMatch: 'Password entered does not match',
-  //     });
-  //     return false;
-  //   } else {
-  //     return enteredPassword.value == this.user[0].password;
-  //   }
-  // };
-
   onSubmit = () => {
-    this.router.navigateByUrl('/dashboard')
-    console.log("Clicked")
-
-    // const username = this.fg.controls.username;
-    // const password = this.fg.controls.password;
-
-    // password.setErrors({ notMatch: null });
-    // password.updateValueAndValidity();
-
-    // setTimeout(() => {
-    //   this.checkLoginCredentials(username, password)
-    //     ? this.router.navigateByUrl('/dashboard')
-    //     : password.setErrors({ notMatch: 'Password entered does not match' });
-    // }, 500);
+    this.router.navigateByUrl('/dashboard');
   };
 }
